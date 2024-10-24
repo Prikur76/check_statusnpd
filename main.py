9fc71ed87e98fb2ff8f4e44bca56997a66d6287f
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2024-10-24 10:01:12 +0300

    merge

--- main.py
+++ main.py
@@ -100,11 +100,8 @@ def check_self_employment_status(inn: str, max_attempts: int = 3) -> tuple[bool,
     request_date = datetime.now(
         pytz.timezone('Europe/Moscow')).strftime('%Y-%m-%d')
     attempt = 0
-<<<<<<< HEAD
     is_self_employed = False
     message = ""
-=======
->>>>>>> b8b2ea1a789f7dd12911b38bf3d034f4e6354834
 
     while attempt < max_attempts:
         try:
@@ -138,10 +135,7 @@ def check_self_employment_status(inn: str, max_attempts: int = 3) -> tuple[bool,
             logger.error("HTTP error: %s", http_err)
             break
 
-<<<<<<< HEAD
     time.sleep(31)
-=======
->>>>>>> b8b2ea1a789f7dd12911b38bf3d034f4e6354834
     return is_self_employed, message, request_date
 
 
